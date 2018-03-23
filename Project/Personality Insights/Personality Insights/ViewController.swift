f0a2c3fd92f4a9147b73b3b39aac7da1ced852d6
shanakor <n.rammerstorfer@icloud.com>
2018-03-23 09:58:51 +0100

    Created project.

---
+++ Project/Personality Insights/Personality Insights/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Personality Insights
+//
+//  Created by Niklas Rammerstorfer on 23.03.18.
+//  Copyright © 2018 Niklas Rammerstorfer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
